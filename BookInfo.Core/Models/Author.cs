e4938dcfccca36506913546e4400035222047cd3
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-11-20 23:06:14 +0300

    Models added

---
+++ BookInfo.Core/Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace BookInfo.Core.Models
+{
+    public class Author
+    {
+        public Author()
+        {
+            Books = new Collection<Book>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
---
+++ BookInfo.Core/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookInfo.Core.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Page { get; set; }
+        public bool IsDeleted { get; set; }
+        public virtual Author Author { get; set; }
+
+    }
+}
