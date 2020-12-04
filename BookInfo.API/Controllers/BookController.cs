68088972f4b8e2d18c0591b5d25f1afee8fa40a4
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-12-04 16:17:30 +0300

    BookController methods added

--- BookInfo.API/Controllers/BookController.cs
+++ BookInfo.API/Controllers/BookController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookInfo.Core.Models;
 using BookInfo.Core.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -22,6 +23,34 @@ namespace BookInfo.API.Controllers
         {
             var books = await _bookService.GetAllAsync();
             return Ok(books);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var book = await _bookService.GetByIdAsync(id);
+            return Ok(book);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            await _bookService.AddAsync(book);
+            return Created("", book);
+        }
+        [HttpPut]
+        public IActionResult Update([FromBody] Book book)
+        {
+            _bookService.Update(book);
+            return NoContent();
+        }
+        [HttpDelete]
+        public IActionResult Delete([FromBody] Book book)
+        {
+            _bookService.Delete(book);
+            return NoContent();
         }
     }
 }
