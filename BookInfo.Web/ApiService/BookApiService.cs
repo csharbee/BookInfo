fd096ddd977f453402ed6c1e7e7cf806edd1d892
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-12-10 20:49:03 +0300

    Mvc project added.
    This project will consume to API

---
+++ BookInfo.Web/ApiService/BookApiService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BookInfo.Web.ApiService
+{
+    public class BookApiService
+    {
+        private readonly HttpClient _httpClient;
+        public BookApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
