5cbbac477a0fbf951d56c77b4e2915e67c2b75e7
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-12-10 20:12:19 +0300

    Global ExceptionHandler added

--- BookInfo.API/Startup.cs
+++ BookInfo.API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookInfo.API.DTOs;
 using BookInfo.API.Filters;
 using BookInfo.Core;
 using BookInfo.Core.Repositories;
@@ -10,7 +11,9 @@ using BookInfo.Data;
 using BookInfo.Data.Repositories;
 using BookInfo.Service.Services;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -19,6 +22,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft;
+using Newtonsoft.Json;
 
 namespace BookInfo.API
 {
@@ -68,7 +72,29 @@ namespace BookInfo.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseExceptionHandler(config =>
+            {
+                config.Run(async context =>
+                {
+                    context.Response.StatusCode = 500;
+                    context.Response.ContentType = "application/json";
+                    var errorFeature = context.Features.Get<IExceptionHandlerFeature>();
 
+                    if (errorFeature != null)
+                    {
+                        var ex = errorFeature.Error;
+
+                        ErrorDto errorDto = new ErrorDto();
+                        errorDto.Status = 500;
+                        errorDto.Errors.Add(ex.Message);
+
+                        await context.Response.WriteAsync(JsonConvert.SerializeObject(errorDto));
+                    }
+
+                });
+            }
+
+            );
             app.UseHttpsRedirection();
 
             app.UseRouting();
